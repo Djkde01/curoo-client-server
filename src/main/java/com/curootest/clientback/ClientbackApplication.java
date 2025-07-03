142f33be5e3eec9d9e95b267e6a265b803c9576a
Djkde01 <sergio.estrella@utp.edu.co>
2025-07-03 14:53:44 -0500

    initial config

---
+++ src/main/java/com/curootest/clientback/ClientbackApplication.java
@@ -0,0 +1,13 @@
+package com.curootest.clientback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curootest/clientback/ClientbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curootest.clientback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
