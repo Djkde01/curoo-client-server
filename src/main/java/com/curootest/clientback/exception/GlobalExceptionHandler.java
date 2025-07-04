46450007c37476746d4c3f6ae589edf1cc8c451b
Djkde01 <sergio.estrella@utp.edu.co>
2025-07-04 16:45:07 -0500

    Add global exception handling for user-related errors

---
+++ src/main/java/com/curootest/clientback/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.curootest.clientback.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ResponseEntity<String> handleInvalidPasswordException(InvalidPasswordException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    // You can add more exception handlers here for other custom exceptions or generic exceptions
+}
